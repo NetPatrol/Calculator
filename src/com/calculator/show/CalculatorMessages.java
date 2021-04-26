d85b43aed47cf443148ecbc293e43afe82d8c13b
NetPatrol <madraddog@gmail.com>
2021-04-26 23:58:33 +0300

    Done

--- src/com/calculator/show/CalculatorMessages.java
+++ src/com/calculator/show/CalculatorMessages.java
@@ -13,8 +13,10 @@ public class CalculatorMessages {
     public static void choiceMenu() {
         System.out.println(
         Color.ANSI_CYAN+"   =================================================================\n" +Color.ANSI_RESET+
-        Color.ANSI_CYAN+"   |\t\t    "+Color.ANSI_YELLOW+"1........ Продолжить          "+Color.ANSI_RESET + Color.ANSI_CYAN+"    \t\t   |\n"+
-        Color.ANSI_CYAN+"   |\t\t    "+Color.ANSI_YELLOW+"2.......... Выход          "+Color.ANSI_RESET + Color.ANSI_CYAN+"\t\t\t   |\n"+
+        Color.ANSI_CYAN+"   +                                                               +\n" +Color.ANSI_RESET+
+        Color.ANSI_CYAN+"   +\t\t    "+Color.ANSI_YELLOW+"Нажмите Enter, чтобы продолжить"+Color.ANSI_RESET + Color.ANSI_CYAN+"    \t\t   +\n"+
+        Color.ANSI_CYAN+"   +\t\t    "+Color.ANSI_YELLOW+"Для завершения нажмите Q"+Color.ANSI_RESET + Color.ANSI_CYAN+"\t\t\t   +\n"+
+        Color.ANSI_CYAN+"   +                                                               +\n" +Color.ANSI_RESET+
         Color.ANSI_CYAN+"   =================================================================\n"+Color.ANSI_RESET);
     }
 
@@ -27,27 +29,28 @@ public class CalculatorMessages {
     }
 
     public static void helloUser() {
-
         System.out.println();
         System.out.println();
         System.out.println(
-                " ========================================================================\n"+
-                " +                                                                      +\n"+
-                " +"+Color.ANSI_YELLOW+"                 Calculator v.1.0 by N3tHunter"+Color.ANSI_RESET+"                        +\n" +
-                " +                                                                      +\n" +
-                "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  +\n" +
-                "+                                                                    +  +\n" +
-                "+        Калькулятор получает на вход два числа  от 0 до 10          +  +\n" +
-                "+        и производит арифметические вычисления такие как:           +  +\n" +
-                "+        - сложение;                                                 +  +\n" +
-                "+        - вычитание;                                                +  +\n" +
-                "+        - умножение;                                                +  +\n" +
-                "+        - деление;                                                  +  +\n" +
-                "+        Кроме того, данный калькулятор может производить            +  +\n" +
-                "+        вычисления посредством ввода римских цифр от I дл X.        +  +\n" +
-                "+        Римских цифры вводите латиницей и включите CapsLock.        +===\n" +
-                "+                                                                    +== \n" +
-                "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                                                    " =====================================================================\n"+
+                                                    " +                                                                   +\n"+
+                             " +"+Color.ANSI_YELLOW+"                 Calculator v.1.0 by N3tHunter                     "+Color.ANSI_RESET+"+\n" +
+                                                    " +                                                                   +\n" +
+                                                    " +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n" +
+                                                    " +                                                                   +\n" +
+                                                    " +       Калькулятор получает на вход два числа  от 0 до 10          +\n" +
+                                                    " +       и производит арифметические следующие вычисления:           +\n" +
+                                                    " +                                                                   +\n" +
+                                                    " +       +  сложение                                                 +\n" +
+                                                    " +       - вычитание                                                 +\n" +
+                                                    " +       * умножение                                                 +\n" +
+                                                    " +       /   деление                                                 +\n" +
+                                                    " +                                                                   +\n" +
+                                                    " +       Кроме того, данный калькулятор может производить            +\n" +
+                                                    " +       вычисления посредством ввода римских цифр от I дл X.        +\n" +
+                                                    " +       Римские цифры вводите латиницей и включите CapsLock.        +\n" +
+                                                    " +                                                                   +\n" +
+                                                    " +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
     }
 
     public static void printArabResult(int num){
